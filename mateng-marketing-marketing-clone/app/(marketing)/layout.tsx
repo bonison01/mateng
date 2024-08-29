2969a87d83d659ec319735adcabe0e92c56792af
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-08-29 10:59:15 +0530

    Update layout.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/layout.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/layout.tsx
@@ -1,9 +1,21 @@
-import MarketingLayout from '@/components/MarketingLayout';
+import { Poppins } from "next/font/google";
+import { Navbar } from "./_components/navbar";
+import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["900", "800", "700", "500", "400", "300", "200", "100", "600"],
+});
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <MarketingLayout>
-      {children}
-    </MarketingLayout>
+    <div className={cn("h-full bg-white", font.className)}>
+      <Navbar />
+      <main className="bg-white">{children}</main>
+      <Toaster />
+    </div>
   );
-}
+};
+
+export default MarketingLayout;
