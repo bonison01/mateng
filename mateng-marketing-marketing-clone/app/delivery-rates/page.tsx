48327ba194ba2814589fad299fb0d6cbb253096a
bonison01 <bonisonkhumbongmayum@gmail.com>
2024-10-09 15:28:47 +0530

    sg

--- mateng-marketing-marketing-clone/app/delivery-rates/page.tsx
+++ mateng-marketing-marketing-clone/app/delivery-rates/page.tsx
@@ -1,14 +1,12 @@
-// /app/delivery-rates/page.tsx
-
-'use client'; // Add this line if page.tsx uses client-side features
-
-import React from 'react';
-import DeliveryRates from './DeliveryRates';
+import { MainInstantRate } from "./_components/instant-delivery-rate";
+import { OtherRate } from "./_components/other-delivery-rates";
+import styles from "./page.module.css"; // Import the custom CSS
 
 export default function DeliveryRatesPage() {
   return (
-    <div>
-      <DeliveryRates />
-    </div>
+    <main className={`${styles.flex} ${styles.mainContainer}`}>
+      <MainInstantRate /> 
+      <OtherRate />
+    </main>
   );
 }
