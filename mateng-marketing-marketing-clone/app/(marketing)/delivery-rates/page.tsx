72c6bec16d8f96c0df16360bf33bd5e40541b53c
bonison01 <bonisonkhumbongmayum@gmail.com>
2024-10-09 13:14:51 +0530

    new

--- mateng-marketing-marketing-clone/app/(marketing)/delivery-rates/page.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/delivery-rates/page.tsx
@@ -1,13 +1,17 @@
-
+import { FromAutocomplete } from "./_components/from-autocomplete";
+import { ToAutocomplete } from "./_components/to-autocomplete";
 import { MainInstantRate } from "./_components/instant-delivery-rate";
 import { OtherRate } from "./_components/other-delivery-rates";
+import './styles/globals.css'; // Import global styles
 
 export default function DeliveryRatesPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <div className="container">
+      <h1>Delivery Rates</h1>
+      <FromAutocomplete />
+      <ToAutocomplete />
       <MainInstantRate />
       <OtherRate />
-
-    </main>
+    </div>
   );
 }
