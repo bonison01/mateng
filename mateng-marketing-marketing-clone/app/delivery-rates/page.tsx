1f47cf68939539ea378ef13f3a049e03f5fe7034
bonison01 <bonisonkhumbongmayum@gmail.com>
2024-10-09 14:15:36 +0530

    new

--- mateng-marketing-marketing-clone/app/delivery-rates/page.tsx
+++ mateng-marketing-marketing-clone/app/delivery-rates/page.tsx
@@ -1,15 +1,14 @@
-import { QuickLink } from "../_components/quick-link-section";
-import { Footer } from "../_components/footer-section";
-import { MainInstantRate } from "./_components/instant-delivery-rate";
-import { OtherRate } from "./_components/other-delivery-rates";
+// /app/delivery-rates/page.tsx
+
+'use client'; // Add this line if page.tsx uses client-side features
+
+import React from 'react';
+import DeliveryRates from './DeliveryRates';
 
 export default function DeliveryRatesPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <MainInstantRate /> 
-      <OtherRate />
-      <QuickLink currentPage="delivery-rates"/>
-      <Footer />
-    </main>
+    <div>
+      <DeliveryRates />
+    </div>
   );
 }
